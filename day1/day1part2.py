0ea6219e009d65ce0a5d55e188effde02e4b3be9
abdulatif ahdab <abdullatif@MacBook-Air-abdulatif.local>
2025-12-05 00:24:43 +0200

    solved day 1 part 2

--- day1/day1part2.py
+++ day1/day1part2.py
@@ -1,78 +1,50 @@
-# DIAL_VALUE = 50
-# zero_times = 0
-
-
-# def ToLeftOrRight(string, dial_value, zero_times):
-
-#     string_splited = string.split("\n")
-#     new_position = None
-#     for i in string_splited:
-#         print(i)
-#         i_letter = i[0]
-#         i_number = int(i[1:])
-#         if i_letter.lower() == "r":
-#             new_position = (dial_value + i_number) % 100
-#             dial_value = new_position
-#             print(new_position, "new_position", "R")
-#             if new_position == 0:
-#                 zero_times += 1
-#         if i_letter.lower() == "l":
-#             new_position = (dial_value - i_number) % 100
-#             dial_value = new_position
-#             print(new_position, "new_position", "L")
-#             if new_position == 0:
-#                 zero_times += 1
-
-#         print("zero_times m3lmi", zero_times)
-
-
-# with open("day1puzzle.txt", "r") as file:
-#     content = file.read()
-#     print(ToLeftOrRight(content, DIAL_VALUE, zero_times))
-
-# part 2
 DIAL_VALUE = 50
-zero_times = 0
+
+def count_zeros_crossed(start, distance, direction):
+    """Count how many times we pass through 0 during a rotation"""
+    count = 0
+    
+    if direction == 'r':
+        # Going right
+        for step in range(1, distance + 1):
+            if (start + step) % 100 == 0:
+                count += 1
+    else:  # direction == 'l'
+        # Going left
+        for step in range(1, distance + 1):
+            if (start - step) % 100 == 0:
+                count += 1
+    
+    return count
 
 
-def ToLeftOrRight(string, dial_value, zero_times):
-
+def ToLeftOrRight(string, dial_value):
     string_splited = string.split("\n")
-    new_position = None
+    zero_times = 0
+    
     for i in string_splited:
-        i_letter = i[0]
+        if not i.strip():
+            continue
+            
+        print(i)
+        i_letter = i[0].lower()
         i_number = int(i[1:])
-
-        if i_letter.lower() == "r":
-            new_position = (dial_value + i_number) % 100
-            i_letter_2 = i_number//100
-
-            for j in range(1, int(i_letter_2)):
-                h = (dial_value + i_number) % 100
-                if h == 0:
-                    zero_times += 1
-                print(h, zero_times, "h_and_zero_teims")
-            # print(new_position, "new pos")
-            dial_value = new_position
-            # print(new_position, "new_position", "R")
-            # if new_position == 0:
-            #     zero_times += 1
-        if i_letter.lower() == "l":
-            new_position = (dial_value - i_number) % 100
-            i_letter_2 = i_number/100
-            print(i_letter_2)
-            for j in range(1, int(i_letter_2)):
-                print(j)
-                h = (dial_value - i_number) % 100
-                if h == 0:
-                    zero_times += 1
-                print(h, zero_times, "h_and_zero_teims")
-            # print(new_position, "new_position", "L")
-            # if new_position == 0:
-            #     zero_times += 1
+        
+        # Count how many times we pass through 0 during this rotation
+        zeros_in_rotation = count_zeros_crossed(dial_value, i_number, i_letter)
+        zero_times += zeros_in_rotation
+        
+        # Update position
+        if i_letter == "r":
+            dial_value = (dial_value + i_number) % 100
+        else:
+            dial_value = (dial_value - i_number) % 100
+            
+        print(f"new position: {dial_value}, zeros crossed: {zeros_in_rotation}, total: {zero_times}")
+    
+    print("Final answer:", zero_times)
 
 
 with open("day1puzzle.txt", "r") as file:
     content = file.read()
-    print(ToLeftOrRight(content, DIAL_VALUE, zero_times))
-# i_letter_2=i_letter/50 important very important
+    ToLeftOrRight(content, DIAL_VALUE)
